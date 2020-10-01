5f1a4758a46a7438ef980075c96c641cac65111e
barbaro reyes <barbaroreyes@barbaros-iMac.local>
2020-09-30 20:22:41 -0500

    sd

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import './component/header/header.css';
-import{Link}from 'react-router-dom';
+// import{Link}from 'react-router-dom';
 import Footer from  './component/footer/footer'
 import Header from './component/header/header';
 
@@ -9,8 +9,8 @@ function App() {
   return (
     <main className="App">
       <div><nav style={{margin:'10px',padding:'10px'}}>
-               <button className='btn1'><Link className='nav' to ="/" target='_black'>Home</Link></button>
-               <button className='btn1'>< Link className='nav' to ="./about-Me" target='_black'>AboutMe</Link></button>
+               <button className='btn1'><a className='nav' href ="/" target='_black'>Home</a></button>
+               <button className='btn1'>< a className='nav' href ="./about-Me" target='_black'>AboutMe</a></button>
                <button className='btn1'>< Link  className='nav' to ="./resume" target='_black'>Resume</Link></button>
                <button className='btn1'> < Link  className='nav' to ="./contact" target='_black'>Contact</Link></button>
                <button className='btn1'> < Link  className='nav' to ="./projects" target='_black'>ProJects</Link></button> 
