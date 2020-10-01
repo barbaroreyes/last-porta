6cec0d43f1defa4774c0505407576eb5e110bffd
barbaro reyes <barbaroreyes@barbaros-iMac.local>
2020-09-30 21:09:55 -0500

    refactory

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import './component/header/header.css';
 import{Link}from 'react-router-dom';
 import Footer from  './component/footer/footer'
 import Header from './component/header/header';
+  
+
 
 function App() {
   return (
     <main className="App">
       <div><nav style={{margin:'10px',padding:'10px'}}>
-               <button className='btn1'><Link className='nav' to ="/" target="\_blank">Home</Link></button>
-               <button className='btn1'>< Link className='nav'   to ="/about-Me" target="\_blank">AboutMe</ Link></button>
-               <button className='btn1'>< Link  className='nav'  to ="./resume" >Resume</Link></button>
-               <button className='btn1'> < Link  className='nav' to ="/contact" >Contact</Link></button>
-               <button className='btn1'> < Link  className='nav' to ="/projects" >ProJects</Link></button> 
+               <button  className='btn1'><Link className='nav' to ="/" >Home</Link></button>
+               <button className='btn1'><Link  className='nav' to ="/about-Me">About-Me</ Link></button>
+               <button className='btn1'><Link  className='nav' to ="/resume">Resume</Link></button>
+               <button className='btn1'><Link  className='nav' to ="/contact">Contact</Link></button>
+               <button className='btn1'><Link  className='nav' to ="/projects">Projects</Link></button> 
     </nav></div> 
        <Header/>
        <Footer/>
--- src/component/contact/contact.js
+++ src/component/contact/contact.js
@@ -13,40 +13,30 @@ import work from '../../work.jpg';
                             <img className='pict' src={work} alt='avatar'style={{width:'200px',marginLeft:'10px'}}/>
                              <p style={{width:'50%',margin:'auto',paddingTop:'1em'}}>With a huge desire to bring smart ideas to the Digital World</p>
                           </div>    
-                   
-                            <h6>Contact me</h6>
+                         <h6>Contact me</h6>
                            
                     <div className='contact-list'>
                         <ul>
-                             <li>
-                                <article style={{fontSize:'25px',fontFamily:'Averia Gruesa Libre'}} >
+                         <li><article style={{fontSize:'25px',fontFamily:'Averia Gruesa Libre'}} >
                                 <i className="fas fa-phone-square">(512)6086054</i>
-                                <li><i class="fas fa-envelope-square"><a href='barbarotrabajo@gmail.com'><p><a className='nav' href="mailto:barbarotrabajo@gmail.com?subject= Hey Barbaro, I'm interested in hearing from you! ">Email Me</a></p></a></i></li>
+                                <li><i class="fas fa-envelope-square"><a href='barbarotrabajo@gmail.com'><p><a className='nav' href="mailto:barbarotrabajo@gmail.com?subject= Hey Barbaro, I'm interested in hearing from you! " target='_black'>Email Me</a></p></a></i></li>
                                 </article>
                             </li>
-                            
-                            <li>
-                                <article
-                                 style={{fontSize:'25px',fontFamily:'Averia Gruesa Libre'}} >
-                                <i class="fab fa-github"><a className='nav' href ='https://github.com/barbaroreyes'>https://github.com/barbaroreyes</a></i>
+                            <li><article style={{fontSize:'25px',fontFamily:'Averia Gruesa Libre'}} >
+                                <i class="fab fa-github"><a className='nav' href ='https://github.com/barbaroreyes' target='_black'>https://github.com/barbaroreyes</a></i>
                                 </article>
                             </li>
-                            <li>
-                                <article
-                                 style={{fontSize:'25px',fontFamily:'Averia Gruesa Libre'}} >
-                                 <i className=" link fab fa-linkedin"><a className='nav' href='https://www.linkedin.com/in/barbaro-reyes-b023aa1a7/'>https://www.linkedin.com/in/barbaro-reyes-b023aa1a7/</a></i>
+                            <li><article style={{fontSize:'25px',fontFamily:'Averia Gruesa Libre'}} >
+                                 <i className=" link fab fa-linkedin"><a className='nav' href='https://www.linkedin.com/in/barbaro-reyes-b023aa1a7/' target='_black'>https://www.linkedin.com/in/barbaro-reyes-b023aa1a7/</a></i>
                                 </article>
                             </li>
                             <div className='socials-liks'>
-                              {/* linkedIn */}
-                                    <a  className ='link' href="https://www.linkedin.com/in/barbaro-reyes" rel='noopener noreferrer' target='_black'>
-                                     <i className=" fab fa-linkedin"></i>
+                                    <a className ='link' href="https://www.linkedin.com/in/barbaro-reyes" rel='noopener noreferrer' target='_black'>
+                                    <i className=" fab fa-linkedin"></i>
                                     </a>
-                                      {/* Gihub */}
-                                      <a href="https://github.com/barbaroreyes" rel='noopener noreferrer' target='_black'>
-                                      <i className="fab fa-github-square"></i>
-                                    </a>
-                         </div>
+                                    <a href="https://github.com/barbaroreyes" rel='noopener noreferrer' target='_black'>
+                                 <i className="fab fa-github-square"></i></a> 
+                            </div>
                         </ul>
                     </div>     
             </div>
