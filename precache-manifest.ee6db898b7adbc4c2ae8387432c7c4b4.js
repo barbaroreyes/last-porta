82fb2cd2b74d21f9f7da06e36ffe01daa1864edb
barbaro reyes <barbaroreyes@barbaros-iMac.attlocal.net>
2021-01-13 00:55:30 -0600

    Updates

---
+++ precache-manifest.ee6db898b7adbc4c2ae8387432c7c4b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1fa4e170e11d7ef12ba11e5f82fb560",
+    "url": "/last-porta/index.html"
+  },
+  {
+    "revision": "1ceac27c20cfc1c8d876",
+    "url": "/last-porta/static/css/main.e6a2d1ac.chunk.css"
+  },
+  {
+    "revision": "6a5fc68a7181ebe1020e",
+    "url": "/last-porta/static/js/2.8e04b7b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/last-porta/static/js/2.8e04b7b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ceac27c20cfc1c8d876",
+    "url": "/last-porta/static/js/main.5fb79232.chunk.js"
+  },
+  {
+    "revision": "203f4c7aa57c483bc570",
+    "url": "/last-porta/static/js/runtime-main.65b0c6cf.js"
+  },
+  {
+    "revision": "4539a6d27f8830f205f81b3eccc44156",
+    "url": "/last-porta/static/media/work.4539a6d2.jpg"
+  }
+]);
