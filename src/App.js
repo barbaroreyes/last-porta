b38d9ff5bb4b1503645ae143f5ccd2f044940389
barbaro reyes <barbaroreyes@barbaros-iMac.attlocal.net>
2021-01-13 01:38:52 -0600

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import './component/header/header.css';
 import{Link}from 'react-router-dom';
-import Footer from  './component/footer/footer'
+import Footer from  './component/footer/footer';
 import Header from './component/header/header';
   
 
@@ -10,13 +10,13 @@ import Header from './component/header/header';
 function App() {
   return (
     <main className="App">
-      <div><nav style={{margin:'10px',padding:'10px'}}>
+      <div><nav>
                <button  className='btn1'><Link className='nav' to ="/" >Home</Link></button>
                <button className='btn1'><Link  className='nav' to ="/about-Me">About-Me</ Link></button>
                <button className='btn1'><Link  className='nav' to ="/resume">Resume</Link></button>
                <button className='btn1'><Link  className='nav' to ="/contact">Contact</Link></button>
-               <button className='btn1'><Link  className='nav' to ="/projects">Projects</Link></button> 
-    </nav></div> 
+               <button className='btn1'><Link  className='nav' to ="/projects">Projects</Link></button>
+               </nav></div> 
        <Header/>
        <Footer/>
     </main>
